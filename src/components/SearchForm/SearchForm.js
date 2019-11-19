40ac5b651ba0d5c4b9d3a1d40c11d07d39c43b1d
Ben Firth <bfirth93@gmail.com>
2019-11-19 13:46:15 -0700

    Add styling to search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,6 +5,8 @@ const SearchForm = () => {
   return (
     <header className='searchForm'>
       <h1>What's New!?</h1>
+      <input type='text' placeholder='Search Articles by Keyword'/>
+      <button className='searchButton'>Enter</button>
     </header>
   )
 }
