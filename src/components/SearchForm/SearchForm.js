678ce65d7fcee4315204afc92bfd559b0e596184
Ben Firth <bfirth93@gmail.com>
2019-11-19 16:22:49 -0700

    Add filter to search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './SearchForm.css';
 
-const SearchForm = () => {
+const SearchForm = ({ searchHandler }) => {
   return (
     <header className='searchForm'>
       <h1>What's New!?</h1>
-      <input type='text' placeholder='Search Articles by Keyword'/>
-      <button className='searchButton'>Enter</button>
+      <form>
+        <input
+          id='search'
+          type='text' 
+          placeholder='Filter Articles by Keyword' 
+        />
+        <button 
+          type='button'
+          className='searchButton'
+          onClick={() => searchHandler(document.getElementById('search').value)}>Enter
+        </button>
+      </form>
     </header>
   )
 }
