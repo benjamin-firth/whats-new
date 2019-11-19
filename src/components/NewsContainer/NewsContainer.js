f03194cb81462140c9a53a9ca35bc8e27c15df8e
Ben Firth <bfirth93@gmail.com>
2019-11-18 21:46:03 -0700

    Create newscontainer component

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({ img, headline, description, url }) => {
+  return (
+    <article>
+      <img src={img} alt='Article header background'/>
+      <h3>{headline}</h3>
+      <p>{description}</p>
+      <a href={url}>Link to Article</a>
+    </article>
+  )
+}
 
 export default NewsContainer;
