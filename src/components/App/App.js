5b011f7fc4aea4f29b8686dda6726dd49ac4b0fa
Ben Firth <bfirth93@gmail.com>
2019-11-19 00:02:11 -0700

    Add button functionality for different news types

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
-import NewsArticle from '../NewsArticle/NewsArticle';
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      currentPage: 'local',
+      local,
+      entertainment,
+      health,
+      science,
+      technology
     }
   }
 
   render () {
     return (
       <div className="app">
-        <NewsContainer props={this.state.local} />
+        <button onClick={() => this.setState({ currentPage: 'science' })}>Science News!</button>
+        <button onClick={() => this.setState({ currentPage: 'entertainment' })}>Entertainment News!</button>
+        <button onClick={() => this.setState({ currentPage: 'health' })}>Health News!</button>
+        <button onClick={() => this.setState({ currentPage: 'technology' })}>technology News!</button>
+        <button onClick={() => this.setState({ currentPage: 'local' })}>Local News!</button>
+        <NewsContainer props={this.state[this.state.currentPage]} />
       </div>
     );
   }
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -3,17 +3,19 @@ import './NewsContainer.css';
 import NewsArticle from '../NewsArticle/NewsArticle';
 
 const NewsContainer = ({ props }) => {
+  const newsArticles = props.map(article => {
+    return <NewsArticle 
+      img={article.img}
+      headline={article.headline}
+      description={article.description}
+      url={article.url}
+      key={article.id}
+    />
+  })
+
   return (
     <main className="articleSection">
-      {props.map(article => {
-        return <NewsArticle 
-          img={article.img}
-          headline={article.headline}
-          description={article.description}
-          url={article.url}
-          key={article.id}
-        />
-      })}
+      {newsArticles}
     </main>
   )
 }
