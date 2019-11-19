75802c60871ab2518dcfab74e72c74a0c0839401
Ben Firth <bfirth93@gmail.com>
2019-11-18 23:04:47 -0700

    Created newsarticle component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = ({ img, headline, description, url }) => {
+  return (
+    <article className="articleBox">
+      <img src={img} alt='Article header background' className="articlePic"/>
+      <h3>{headline}</h3>
+      <hr/>
+      <p>{description}</p>
+      <a href={url}>Link to Article</a>
+    </article>
+  )
+}
 
 export default NewsArticle;
