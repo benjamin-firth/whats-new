055be5b82e15067619cd0043c0c83b475e64967f
Ben Firth <bfirth93@gmail.com>
2019-11-19 08:27:59 -0700

    Add menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+  return (
+    <nav className='navBar'>
+      <button onClick={() => this.setState({ currentPage: 'science' })}>Science News!</button>
+      <button onClick={() => this.setState({ currentPage: 'entertainment' })}>Entertainment News!</button>
+      <button onClick={() => this.setState({ currentPage: 'health' })}>Health News!</button>
+      <button onClick={() => this.setState({ currentPage: 'technology' })}>technology News!</button>
+      <button onClick={() => this.setState({ currentPage: 'local' })}>Local News!</button>
+    </nav>
+  )
+}
 
 export default Menu;
