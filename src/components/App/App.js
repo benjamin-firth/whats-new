609cf01c94b40cf4b21a8741c5f84931617a9264
Ben Firth <bfirth93@gmail.com>
2019-11-18 21:46:45 -0700

    Add newscontainers to main section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import NewsContainer from '../NewsContainer/NewsContainer';
 
 class App extends Component {
   constructor() {
@@ -8,12 +9,21 @@ class App extends Component {
     this.state = {
       local
     }
+    this.newsArticle = this.state.local.map(article => {
+      return <NewsContainer 
+        img={article.img}
+        headline={article.headline}
+        description={article.description}
+        url={article.url}
+        key={article.id}
+      />
+    })
   }
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        {this.newsArticle}
       </div>
     );
   }
