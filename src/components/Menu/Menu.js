01e46b397f844f709993a811497a548c6edb03ab
Ben Firth <bfirth93@gmail.com>
2019-11-19 19:51:32 -0700

    Add menu styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,11 +4,11 @@ import './Menu.css'
 const Menu = ({ clickHandler }) => {
   return (
     <nav className='navBar'>
-      <button id='science' onClick={event => clickHandler(event)}>Science News!</button>
-      <button id='entertainment' onClick={event => clickHandler(event)}>Entertainment News!</button>
-      <button id='health' onClick={event => clickHandler(event)}>Health News!</button>
-      <button id='technology' onClick={event => clickHandler(event)}>Technology News!</button>
-      <button id='local' onClick={event => clickHandler(event)}>Local News!</button>
+      <button id='science' onClick={event => clickHandler(event)}>Science</button>
+      <button id='entertainment' onClick={event => clickHandler(event)}>Entertainment</button>
+      <button id='health' onClick={event => clickHandler(event)}>Health</button>
+      <button id='technology' onClick={event => clickHandler(event)}>Technology</button>
+      <button id='local' onClick={event => clickHandler(event)}>Local</button>
     </nav>
   )
 }
