26150f163b867469038311f5ca6386e59afc26e4
Ben Firth <bfirth93@gmail.com>
2019-11-22 09:36:20 -0700

    Add change current page method testing in app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,10 +3,21 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<App />);
+  let wrapper;
+  beforeEach(() => {
+  wrapper = shallow(<App />);
+  })
 
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should update state when changeCurrentPage is called', () => {
+    const mockEvent = {target: {id: 'local'}};
+    const expected = 'local';
+
+    wrapper.instance().changeCurrentPage(mockEvent);
+    expect(wrapper.state('currentPage')).toEqual(expected);
   })
 })
 
