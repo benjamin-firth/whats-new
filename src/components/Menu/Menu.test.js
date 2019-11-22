406191938ac055f255bcc49974b87f4e1a996f4d
Ben Firth <bfirth93@gmail.com>
2019-11-22 09:35:19 -0700

    Add testing for menu

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -3,11 +3,36 @@ import { shallow } from 'enzyme';
 import Menu from './Menu';
 
 describe('Menu', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<Menu 
-      clickHandler={jest.fn()}
-    />);
+  let wrapper;
+  let mockClickHandler;
 
+  beforeEach(() => {
+    mockClickHandler = jest.fn();
+    wrapper = shallow(<Menu 
+      clickHandler={mockClickHandler}
+    />)
+  })
+
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should call the clickHandler prop method when button is clicked', () => {
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(mockClickHandler).toHaveBeenCalled();
+
+    wrapper.find('button').at(1).simulate('click');
+    expect(mockClickHandler).toHaveBeenCalled();
+
+    wrapper.find('button').at(2).simulate('click');
+    expect(mockClickHandler).toHaveBeenCalled();
+
+    wrapper.find('button').at(3).simulate('click');
+    expect(mockClickHandler).toHaveBeenCalled();
+
+    wrapper.find('button').at(4).simulate('click');
+    expect(mockClickHandler).toHaveBeenCalled();
+
   })
 })
