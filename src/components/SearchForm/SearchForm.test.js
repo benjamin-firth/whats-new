989b812cbafdc98eb887c76a1c585a5dbc0559e5
Ben Firth <bfirth93@gmail.com>
2019-11-22 09:35:27 -0700

    Add testing for search form

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -3,10 +3,29 @@ import { shallow  } from 'enzyme';
 import SearchForm from './SearchForm';
 
 describe('SearchForm', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<SearchForm 
-      searchHandler={jest.fn()}
+  let wrapper;
+  let mockSearchHandler;
+  
+  beforeEach(() => {
+    mockSearchHandler = jest.fn();
+    wrapper = shallow(<SearchForm 
+      searchHandler={mockSearchHandler}
     />)
+  })
+
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update state when handleChange is invoked', () => {
+    const mockEvent = {target: {value: 'test'}};
+    wrapper.instance().handleChange(mockEvent);
+    expect(wrapper.state('keyword')).toEqual('test');
+  })
+
+  it('should call the the searchHandler method when button is clicked', () => {
+
+    wrapper.find('button').simulate('click');
+    expect(mockSearchHandler).toHaveBeenCalled();
+  })
 })
