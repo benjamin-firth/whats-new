7b774e29737b633b8b07e9e4a7091aac8236892c
Ben Firth <bfirth93@gmail.com>
2019-11-22 09:15:27 -0700

    Switch menu to class component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-const SearchForm = ({ searchHandler }) => {
-  return (
-    <header className='searchForm'>
-      <h1>What's<span className='boldTitle'>New?</span></h1>
-      <form>
-        <input
-          id='search'
-          type='text' 
-          placeholder='Filter Articles by Keyword' 
-        />
-        <button 
-          type='button'
-          className='searchButton'
-          onClick={() => searchHandler(document.getElementById('search').value)}>Enter
-        </button>
-      </form>
-    </header>
-  )
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      keyword: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ keyword: event.target.value })
+  }
+
+  render() {
+    return (
+      <header className='searchForm'>
+        <h1>What's<span className='boldTitle'>New?</span></h1>
+        <form>
+          <input
+            id='search'
+            type='text' 
+            placeholder='Filter Articles by Keyword'
+            onChange={this.handleChange}
+          />
+          <button 
+            type='button'
+            className='searchButton'
+            onClick={() => this.props.searchHandler(this.state.keyword)}>Enter
+          </button>
+        </form>
+      </header>
+    )
+  }
 }
 export default SearchForm;
